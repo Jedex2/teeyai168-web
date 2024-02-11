c46a3a3864297987b84ff831ebfbbdec731d53a6
Jedex2 <Apirug.pr@gmail.com>
2024-02-11 23:58:13 +0700

    k

--- slot.py
+++ slot.py
@@ -9,6 +9,18 @@ app = Flask(__name__)
 def slot():
     return render_template('index.html')
 
+@app.route('/login')
+def login():
+    return render_template('loginForm.html')
+
+@app.route('/main')
+def main():
+    return render_template('main.html')
+
+@app.route('/signin')
+def signin():
+    return render_template('signinForm.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
 
